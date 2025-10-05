c5529b47575aee3c3ec22d82e4db9976b7bc7bc9
joshuabenedict-665 <joshuabenedict665@gmail.com>
2025-10-05 14:58:49 +0530

    changed app

--- Plant-Disease-Detection/flask_app/app.py
+++ Plant-Disease-Detection/flask_app/app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, redirect, render_template, request
 from PIL import Image
 import torchvision.transforms.functional as TF
-import CNN
+from . import CNN
 import numpy as np
 import torch
 import pandas as pd
